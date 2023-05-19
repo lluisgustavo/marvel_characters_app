775594435731dc453e81258012ac997a9b94682f
lluisgustavo <lluisgusttavo@gmail.com>
2023-05-19 20:07:05 -0300

    feat: AgentDossier component to show on Dossier

---
+++ src/components/AgentDossier.tsx
@@ -0,0 +1,31 @@
+import { SuperbeingCard } from './SuperbeingCard'
+
+interface Character {
+  id: number
+  name: string
+  description: string
+  thumbnail: {
+    path: string
+    extension: string
+  }
+}
+
+interface AgentDossierProps {
+  characters: Character[]
+}
+
+export function AgentDossier({ characters }: AgentDossierProps) {
+  return (
+    <div className="container mx-auto grid max-h-96 snap-y grid-cols-5 gap-8 p-6">
+      {characters.map((character) => (
+        <SuperbeingCard
+          key={character.id}
+          characterId={character.id}
+          name={character.name}
+          description={character.description}
+          thumbnail={character.thumbnail}
+        />
+      ))}
+    </div>
+  )
+}
