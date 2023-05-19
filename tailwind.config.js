cfd04128dc555d9096d3a4ac92d3b972cc0e0698
lluisgustavo <lluisgusttavo@gmail.com>
2023-05-19 18:22:57 -0300

    style: add fonts to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: 'var(--font-poppins)',
+        classified: 'var(--font-classified)',
+        roboto: 'var(--font-roboto)',
       },
     },
   },
