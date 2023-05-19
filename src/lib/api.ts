4d26345cfc8e322d5431d14e485248c707af3beb
lluisgustavo <lluisgusttavo@gmail.com>
2023-05-19 19:57:07 -0300

    feat: construct call to marvel's api

---
+++ src/lib/api.ts
@@ -0,0 +1,20 @@
+import axios from 'axios'
+import md5 from 'md5'
+
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL as string
+const publicKey = process.env.NEXT_PUBLIC_MARVEL_PUBLIC_KEY as string
+const privateKey = process.env.NEXT_PUBLIC_MARVEL_PRIVATE_KEY as string
+const ts = Number(new Date())
+const hash = md5(ts + privateKey + publicKey)
+
+const api = axios.create({
+  baseURL,
+  params: {
+    ts,
+    apikey: publicKey,
+    hash,
+    limit: 10,
+  },
+})
+
+export default api
