2ab6bf69d5c14ef481c38e54fb6363176ce80761
lluisgustavo <lluisgusttavo@gmail.com>
2023-05-20 13:41:52 -0300

    feat (dossier): add skeleton loading

---
+++ src/app/dossier/loading.tsx
@@ -0,0 +1,22 @@
+export default function Loading() {
+  return (
+    <div
+      role="status"
+      className="h-screen w-full animate-pulse cursor-progress flex-col items-center justify-between space-y-8 pb-12 md:px-12 lg:flex-row"
+    >
+      <div className="flex h-16 w-full flex-col items-center justify-center gap-8 bg-zinc-600 md:flex-row md:gap-0">
+        <div className="h-16 bg-zinc-600"></div>
+        <div className="h-16 bg-zinc-600"></div>
+      </div>
+      <div className="mt-12 h-40 ">
+        <div className="h-16 bg-zinc-600" />
+        <div className="mt-2 h-16 bg-zinc-600" />
+      </div>
+      <div className="container mx-auto grid h-screen grid-cols-1 md:gap-8 lg:grid-cols-2 lg:p-6 xl:grid-cols-3 2xl:grid-cols-5">
+        {[...Array(10)].map((_, index) => (
+          <div key={index} className="h-[500px] bg-zinc-600" />
+        ))}
+      </div>
+    </div>
+  )
+}
