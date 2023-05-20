e29723e54b463bc3c49811c91cee7e39a7475bdc
lluisgustavo <lluisgusttavo@gmail.com>
2023-05-20 03:22:40 -0300

    refactor: centralize api calls

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,6 @@
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 import md5 from 'md5'
+import { notFound } from 'next/navigation'
 
 const baseURL = 'http://gateway.marvel.com/v1/public/'
 const publicKey = process.env.NEXT_PUBLIC_MARVEL_PUBLIC_KEY as string
@@ -16,5 +17,71 @@ const api = axios.create({
     limit: 10,
   },
 })
+
+export const fetchData = async (characterId: number) => {
+  try {
+    const responses = await Promise.all([
+      api.get(`/characters/${characterId}`),
+      api.get(`/characters/${characterId}/comics`),
+      api.get(`/characters/${characterId}/events`),
+      api.get(`/characters/${characterId}/series`),
+    ])
+
+    const characterResult = responses[0].data.data.results[0]
+    const comicsResult = responses[1].data.data.results
+    const eventsResult = responses[2].data.data.results
+    const seriesResult = responses[3].data.data.results
+
+    return {
+      character: characterResult,
+      comics: comicsResult,
+      events: eventsResult,
+      series: seriesResult,
+    }
+  } catch (err) {
+    const axiosError = err as AxiosError
+
+    if (axiosError.response && axiosError.response.status === 404)
+      return notFound()
+
+    console.error(err)
+  }
+}
+
+export const fetchCharacters = async (
+  query: string,
+  characterOffset: number,
+  queryLimit: number,
+) => {
+  const queryParams = query !== '' ? { nameStartsWith: query } : {}
+
+  try {
+    const response = await api.get(`/characters`, {
+      params: { ...queryParams, limit: queryLimit, offset: characterOffset },
+    })
+
+    const {
+      offset,
+      limit,
+      total,
+      count,
+      results,
+    }: {
+      offset: number
+      limit: number
+      total: number
+      count: number
+      results: any[]
+    } = response.data.data
+
+    if (results.length === 0) notFound()
+    return {
+      pagination: { offset, limit, total, count },
+      characters: results,
+    }
+  } catch {
+    throw new Error('Error fetching characters')
+  }
+}
 
 export default api
