1b77cfe4d1b9257b798db65fbb8a4046ad3a69e3
lluisgustavo <712020:2881cd57-cc52-4a44-a2b4-17a5008a89dc@bots.bitbucket.org>
2023-05-21 01:19:54 -0300

    feat: not found page added

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,27 +1,18 @@
 'use client'
-import { useRouter } from 'next/navigation'
 
 export default function NotFound() {
-  const router = useRouter()
-
   return (
     <div className="flex h-80 flex-col items-center justify-center space-y-12">
       <h1 className="text-4xl font-semibold text-white">
         ERROR 404: Access Denied, Agent!
       </h1>
       <p className="text-xl text-gray-300">
-        The top secret S.H.I.E.L.D. database you are looking for has been
+        The top secret S.H.I.E.L.D. database page you are looking for has been
         compromised by Hydra.
       </p>
       <p className="mt-4 text-xl text-gray-300">
         Please report this incident to your superior immediately and stay alert.
       </p>
-      <div
-        onClick={() => router.back()}
-        className="mt-8 text-lg text-gray-400 underline hover:text-gray-200"
-      >
-        Return
-      </div>
     </div>
   )
 }
