a38c8a28ef4fd741958cb371e621b073ca482703
lluisgustavo <712020:2881cd57-cc52-4a44-a2b4-17a5008a89dc@bots.bitbucket.org>
2023-05-21 14:44:36 -0300

    fix (carousel): logo width responsive

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -73,8 +73,12 @@ export function Carousel({ images }: CarouselProps) {
 
   if (!images || images.length === 0) {
     return (
-      <div className="flex h-full min-h-[600px] w-full flex-col items-center justify-center space-y-24">
-        <Image className="w-3/12" src={ShieldLogo} alt="Nothing Found"></Image>
+      <div className="flex h-full min-h-[600px] w-full flex-col items-center justify-center space-y-24 sm:py-8">
+        <Image
+          className="w-3/4  sm:w-3/6"
+          src={ShieldLogo}
+          alt="Nothing Found"
+        ></Image>
         <p className="text-center text-3xl">No records found in our database</p>
       </div>
     )
