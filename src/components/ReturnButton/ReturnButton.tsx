98766c020b5726f18a0db8649f0d9eb5d6fd366c
lluisgustavo <712020:2881cd57-cc52-4a44-a2b4-17a5008a89dc@bots.bitbucket.org>
2023-05-22 00:50:11 -0300

    refactor (dossier): separate return button

--- src/components/ReturnButton/ReturnButton.tsx
+++ src/components/ReturnButton/ReturnButton.tsx
@@ -1,13 +1,20 @@
-export interface ReturnButtonProps {
-  onClick: () => void
-}
+'use client'
 
-export function ReturnButton({ onClick }: ReturnButtonProps) {
+import { deleteCookie } from 'cookies-next'
+import { useRouter } from 'next/navigation'
+
+export function ReturnButton() {
+  const router = useRouter()
+  const handleReturnClick = () => {
+    deleteCookie('isAgent')
+    router.push('/')
+  }
+
   return (
     <div className="flex w-full items-center justify-center">
       <button
         className="mb-12 text-center text-3xl text-zinc-300"
-        onClick={onClick}
+        onClick={handleReturnClick}
       >
         Encrypt Database
       </button>
--- src/components/ReturnButton/index.spec.tsx
+++ src/components/ReturnButton/index.spec.tsx
@@ -1,33 +1,18 @@
 import '@testing-library/jest-dom'
-import { render, screen, fireEvent } from '@testing-library/react'
-import { ReturnButton, ReturnButtonProps } from './ReturnButton'
+import { render, screen } from '@testing-library/react'
+import { ReturnButton } from './ReturnButton'
+import React from 'react'
+
+jest.mock('next/navigation', () => ({
+  useRouter: jest.fn(),
+}))
 
 describe('ReturnButton', () => {
-  let onClickMock: jest.Mock<void, []>
-  let props: ReturnButtonProps
-
-  beforeEach(() => {
-    onClickMock = jest.fn()
-    props = {
-      onClick: onClickMock,
-    }
-  })
-
   test('renders button with correct text', () => {
-    render(<ReturnButton {...props} />)
+    render(<ReturnButton />)
 
     const buttonElement = screen.getByText('Encrypt Database')
 
     expect(buttonElement).toBeInTheDocument()
-  })
-
-  test('triggers onClick callback when button is clicked', () => {
-    render(<ReturnButton {...props} />)
-
-    const buttonElement = screen.getByText('Encrypt Database')
-
-    fireEvent.click(buttonElement)
-
-    expect(onClickMock).toHaveBeenCalled()
   })
 })
