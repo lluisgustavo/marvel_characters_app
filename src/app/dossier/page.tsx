4c347224c67ff8a24a3d1f2e201f36990545acd4
lluisgustavo <712020:2881cd57-cc52-4a44-a2b4-17a5008a89dc@bots.bitbucket.org>
2023-05-22 01:17:58 -0300

    refactor (dossier): dossier fetcher for data

--- src/app/dossier/page.tsx
+++ src/app/dossier/page.tsx
@@ -3,99 +3,17 @@ import { DossierContent } from '@/components/DossierContent/DossierContent'
 import { DossierPagination } from '@/components/DossierPagination/DossierPagination'
 import { ReturnButton } from '@/components/ReturnButton/ReturnButton'
 import { SearchBar } from '@/components/SearchBar/SearchBar'
-import { fetchCharacters } from '@/lib/api/api'
-import { deleteCookie } from 'cookies-next'
-import { useRouter } from 'next/navigation'
-import { useState, useEffect, useRef } from 'react'
 import { LoadingDossier } from './loading'
-import { Character, Pagination } from '@/lib/types'
+import { useDossierData } from '@/components/DossierContent/DossierFetcher'
 
 export default function Dossier() {
-  const router = useRouter()
-  const [characters, setCharacters] = useState<Character[]>([])
-  const [pagination, setPagination] = useState<Pagination>({
-    count: 0,
-    limit: 0,
-    offset: 0,
-    total: 0,
-  })
-  const [query, setQuery] = useState<string>('')
-  const [offset, setOffset] = useState<number>(0)
-  const [queryLimit, setQueryLimit] = useState<number>(10)
-  const [loading, setLoading] = useState<boolean>(false)
-  const oldTotalRef = useRef<number>(0)
-
-  useEffect(() => {
-    // Update the oldTotalRef value after fetching characters
-    oldTotalRef.current = pagination.total
-  }, [pagination.total])
-
-  function handleOffset(direction: 'left' | 'right' | number) {
-    if (typeof direction === 'number') {
-      const clickedOffset = Math.max((direction - 1) * pagination.limit, 0)
-      return setOffset(clickedOffset)
-    }
-
-    const newOffset =
-      direction === 'left'
-        ? Math.max(offset - pagination.limit, 0)
-        : Math.min(
-            offset + pagination.limit,
-            pagination.total - pagination.limit,
-          )
-
-    return setOffset(newOffset)
-  }
-
-  useEffect(() => {
-    const fetchCharactersData = async () => {
-      try {
-        setLoading(true)
-
-        // Reset the offset to 0 with every new search
-
-        const { pagination, characters } = await fetchCharacters(
-          query,
-          offset,
-          queryLimit,
-        )
-
-        if (pagination.total > 0 && pagination.count === 0) {
-          return setOffset(0)
-        }
-
-        setPagination(pagination)
-        setCharacters(characters)
-
-        // Adjust the offset when the total number of characters changes
-        if (oldTotalRef.current !== pagination.total) {
-          // Perform actions or updates specific to when total changes
-          // For example, resetting the offset to 0
-          return setOffset(0)
-        }
-      } catch (error) {
-        console.error(error)
-      } finally {
-        setLoading(false)
-      }
-    }
-
-    fetchCharactersData()
-  }, [offset, query, queryLimit])
-
-  const handleReturnClick = () => {
-    deleteCookie('isAgent')
-    router.push('/')
-  }
-
-  useEffect(() => {
-    setQuery(query)
-  }, [query])
+  const { characters, pagination, loading, handleOffset, setQuery, setLimit } =
+    useDossierData()
 
   return (
     <>
-      <ReturnButton onClick={handleReturnClick} />
-      <SearchBar setQuery={setQuery} setLimit={setQueryLimit} />
+      <ReturnButton />
+      <SearchBar setQuery={setQuery} setLimit={setLimit} />
       {loading ? (
         <LoadingDossier />
       ) : (
---
+++ src/components/DossierContent/DossierFetcher.tsx
@@ -0,0 +1,87 @@
+import { useState, useEffect, useRef } from 'react'
+import { fetchCharacters } from '@/lib/api/api'
+import { Character, Pagination } from '@/lib/types'
+
+export function useDossierData() {
+  const [characters, setCharacters] = useState<Character[]>([])
+  const [pagination, setPagination] = useState<Pagination>({
+    count: 0,
+    limit: 0,
+    offset: 0,
+    total: 0,
+  })
+  const [query, setQuery] = useState<string>('')
+  const [offset, setOffset] = useState<number>(0)
+  const [limit, setLimit] = useState<number>(10)
+  const [loading, setLoading] = useState<boolean>(false)
+  const oldTotalRef = useRef<number>(0)
+
+  useEffect(() => {
+    // Update the oldTotalRef value after fetching characters
+    oldTotalRef.current = pagination.total
+  }, [pagination.total])
+
+  function handleOffset(direction: 'left' | 'right' | number) {
+    if (typeof direction === 'number') {
+      const clickedOffset = Math.max((direction - 1) * pagination.limit, 0)
+      return setOffset(clickedOffset)
+    }
+
+    const newOffset =
+      direction === 'left'
+        ? Math.max(offset - pagination.limit, 0)
+        : Math.min(
+            offset + pagination.limit,
+            pagination.total - pagination.limit,
+          )
+
+    return setOffset(newOffset)
+  }
+
+  useEffect(() => {
+    const fetchCharactersData = async () => {
+      setLoading(true)
+      try {
+        const { pagination, characters } = await fetchCharacters(
+          query,
+          offset,
+          limit,
+        )
+
+        if (pagination.total > 0 && pagination.count === 0) {
+          setOffset(0)
+          return
+        }
+
+        setPagination(pagination)
+        setCharacters(characters)
+
+        // Adjust the offset when the total number of characters changes
+        if (oldTotalRef.current !== pagination.total) {
+          setOffset(0)
+          return
+        }
+        setLoading(false)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    fetchCharactersData()
+  }, [limit, offset, query])
+
+  useEffect(() => {
+    if (offset !== 0 && oldTotalRef.current !== pagination.total) {
+      setOffset(0)
+    }
+  }, [query, limit, offset, pagination.total])
+
+  return {
+    characters,
+    pagination,
+    loading,
+    handleOffset,
+    setQuery,
+    setLimit,
+  }
+}
